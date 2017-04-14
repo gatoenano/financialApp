603ef99ab566ab467bcc8ae7f3e93d3d73c8a674
gatoenano <fedegur@gmail.com>
2017-04-15 01:02:52 +0200

    config changes

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
-
-app.all('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', { root: __dirname });
-});
-
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
