bb9fbb5320e1563dabd31e0fd120852d1356cc6d
gatoenano <fedegur@gmail.com>
2017-04-15 01:46:32 +0200

    config changes

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 // serve angular front end files from root path
-// router.use('/', express.static('dist', { redirect: false }));
+app.use('/', express.static('dist', { redirect: false }));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
