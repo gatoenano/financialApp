6d43125bb3b1bacf94683ae1292448b5d328398d
gatoenano <fedegur@gmail.com>
2017-04-15 01:43:00 +0200

    config changes

--- server.js
+++ server.js
@@ -2,22 +2,25 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist', { redirect: false }));
 // serve angular front end files from root path
 // router.use('/', express.static('dist', { redirect: false }));
-
-// rewrite virtual urls to angular app to enable refreshing of internal pages
-router.get('*', function (req, res, next) {
-    res.sendFile(path.resolve('dist/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+// rewrite virtual urls to angular app to enable refreshing of internal pages
+// router.get('*', function (req, res, next) {
+//     res.sendFile(path.resolve('dist/index.html'));
+// });
 
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
-module.exports = router;
+// module.exports = router;
 //
 // var express = require('express');
 // var path = require('path');
