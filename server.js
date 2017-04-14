1ae1b822a44d09d483ea45a6249b7cc273c1ca18
gatoenano <fedegur@gmail.com>
2017-04-15 01:15:20 +0200

    config changes

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
-});
+app.use(express.static(__dirname + 'dist'));
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
