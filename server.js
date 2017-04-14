fecb95bb76cbb6d35c7cee3966ebffaac04aa559
gatoenano <fedegur@gmail.com>
2017-04-15 00:43:16 +0200

    config changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 const express = require('express');
 const app = express();
+app.use(router.routes());
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
